30bd45bd058780e0bab8b6cf4b2b1c76e567ffeb
carlos <carloslonghi.cl@gmail.com>
2023-09-07 14:26:53 -0300

    feat: update ProductRepository

--- src/repositories/ProductRepository.js
+++ src/repositories/ProductRepository.js
@@ -1,17 +1,40 @@
 const knex = require('../database')
 
 class ProductRepository {
-  async create({ name, description, category_id, img_url, ingredients, price }) {
+  async create({ name, description, category_id, img_url,  price }) {
     const [productId] = await knex('products').insert({
       name,
       description,
       category_id,
       img_url,
-      ingredients,
       price
     })
 
     return { id: productId }
+  }
+
+  async update(id, { name, description, category_id, img_url, price }) {
+    await knex('products').where('id', id).update({
+      name,
+      description,
+      category_id,
+      img_url,
+      price
+    });
+  }
+
+  async getById(id) {
+    const product = await knex('products').where('id', id).first();
+    return product;
+  }
+  
+  async getAllByCategory(category_id) {
+    const products = await knex('products').where('category_id', category_id);
+    return products;
+  }
+
+  async delete(id) {
+    await knex('products').where('id', id).del();
   }
 }
 
