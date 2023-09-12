f90ddc2797bc25a53a887ab9c4fb4431457718f6
carlos <carloslonghi.cl@gmail.com>
2023-09-11 23:54:20 -0300

    feat: add session authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express")
 const usersRouter = require("./users.routes")
 const productRouter = require("./product.routes")
 const categoryRouter = require("./category.routes")
+const sessionRouter = require("./session.routes")
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/product', productRouter)
 routes.use('/category', categoryRouter)
+routes.use('/session', sessionRouter)
 
 module.exports = routes
