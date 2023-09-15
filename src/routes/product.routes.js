2a8e402a924b0dd7578ad225bcf05e901c514052
carlos <carloslonghi.cl@gmail.com>
2023-09-14 21:51:52 -0300

    feat: use authentication in route product

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express')
 const ProductController = require('../controllers/ProductController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const productRoutes = Router()
 const productController = new ProductController()
+
+productRoutes.use(ensureAuthenticated)
 
 productRoutes.post('/', productController.create)
 productRoutes.get('/', productController.index)
