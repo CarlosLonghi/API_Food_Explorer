896b09c1a7a1e10c3f33b02105311207cea1a1ed
carlos <carloslonghi.cl@gmail.com>
2023-09-25 23:30:01 -0300

    build: install and add Cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require("dotenv/config")
 
 const AppError = require("./utils/AppError")
 const express = require("express")
+const cors = require("cors")
 const uploadConfig = require('./configs/upload')
 
 const routes = require('./routes')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
